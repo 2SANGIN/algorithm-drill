bda2d5e8521e0e6308da117900c172034f0fedb0
silee <silee9019@gmail.com>
2022-01-26 22:18:52 +0900

    reformat

--- codewars/doesMyNumberLookBigInThis.js
+++ codewars/doesMyNumberLookBigInThis.js
@@ -5,7 +5,7 @@
  */
 function narcissistic(value) {
     const digits = [];
-    let number = value;
+    let number   = value;
     while (number > 0) {
         digits.unshift(number % 10);
         number = Math.floor(number / 10);
@@ -19,9 +19,9 @@ function narcissistic(value) {
 const {assert} = require("chai");
 
 describe("Does my number look big in this?", function () {
-  test(7, true, "7 is narcissistic");
-  test(371, true, "371 is narcissistic");
-  test(1652, true, "1652 is not narcissistic");
+    test(7, true, "7 is narcissistic");
+    test(371, true, "371 is narcissistic");
+    test(1652, true, "1652 is not narcissistic");
 });
 
 function test(parameter, expected, title) {
